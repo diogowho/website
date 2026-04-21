f9061a00c0950b433cd6184ed043640605621eb4
Diogo <hello@diogocastro.net>
2026-04-21 15:42:16 +0100

    deploy to cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,18 @@ import sitemap from "@astrojs/sitemap";
 
 import tailwindcss from "@tailwindcss/vite";
 
+import cloudflare from "@astrojs/cloudflare";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://diogocastro.net",
-
   integrations: [sitemap()],
 
   vite: {
     plugins: [tailwindcss()],
   },
+
+  adapter: cloudflare({
+    imageService: { build: "compile", runtime: "cloudflare-binding" },
+  }),
 });
