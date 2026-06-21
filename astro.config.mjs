3e0111521add56712f33b0540349f97af67c52ae
Diogo <hello@diogocastro.net>
2026-06-21 17:12:31 +0100

    home/socials: add icons

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,14 @@ import { defineConfig } from "astro/config";
 import sitemap from "@astrojs/sitemap";
 import mdx from "@astrojs/mdx";
 import tailwindcss from "@tailwindcss/vite";
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://diogocastro.net",
   output: "static",
 
-  integrations: [sitemap(), mdx()],
+  integrations: [sitemap(), mdx(), icon()],
   vite: {
     plugins: [tailwindcss()],
   },
--- src/consts.ts
+++ src/consts.ts
@@ -3,25 +3,35 @@ export const SELF: { name: string; email: string } = {
   email: "website@diogocastro.net",
 };
 
-export const SOCIALS: { name: string; href: string }[] = [
+export const SOCIALS: { name: string; href: string; icon?: string }[] = [
   {
     name: "Codeberg",
     href: "https://codeberg.org/diogocastro",
+    icon: "devicon-plain:codeberg",
   },
   {
     name: "Mastodon",
     href: "https://hachyderm.io/@iz",
+    icon: "mdi:mastodon",
   },
   {
     name: "Steam",
     href: "https://steamcommunity.com/profiles/76561198950840617",
+    icon: "mdi:steam",
   },
   {
     name: "Signal",
     href: "https://signal.me/#eu/fsBt9oTZ4lxcqA7hHvjot1ft2C91k6ysxLETTqMYf5VECufwnq9C_YlbMkIlQ4ra",
+    icon: "arcticons:signal",
   },
   {
     name: "Matrix",
     href: "https://matrix.to/#/@iz:skji.org",
+    icon: "simple-icons:matrix",
+  },
+  {
+    name: "Email",
+    href: `mailto:${SELF.email}`,
+    icon: "mdi:email",
   },
 ];
